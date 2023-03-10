805a7e505ad135d4a11f39c62ee1fecd59cf9848
Francisken03 <franciskenken.ventura@gmail.com>
2023-03-10 16:22:54 +0800

    finished mobile responsive for 480px

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,3 +15,40 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
+
+/*
+CODE BLOCK FOR TRAVEL PACKAGE LOADER
+*/
+
+// function loadContent(btnId, contentID) {
+    
+// }
+
+// const btnShow = document.querySelectorAll('clickable-card');
+// const abangNaCotainer = document.getElementById('contentContainer');
+
+// btnShow.forEach(function (button) {
+//     button.addEventListener('click', function () {
+    
+//         // let result = document.getElementById('abangNaIframe').src = "baguio.html"
+//         let result = button.getAttribute('data-content');
+
+//         abangNaCotainer.src = result;
+//         console.log(result)
+//     });
+    
+// });
+
+
+
+// var folderPath = "";
+
+// function showContent(btnUrl) {
+//     // var folderPath = btnUrl;
+
+//     console.log(folderPath);
+
+//     var result = document.getElementById('abangNaIframe').src = btnUrl;
+// }
+
